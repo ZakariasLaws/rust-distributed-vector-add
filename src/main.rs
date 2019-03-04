70fbe83b4003bbec42664a45e42248315a5fe749
Zakarias Nordfaldt-Laws <zns200@fs0.cm.cluster>
2019-03-04 16:53:32 +0100

    Added an example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate mpi;
+
+use mpi::traits::*;
+use mpi::request::WaitGuard;
+
 fn main() {
-    println!("Hello, world!");
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let size = world.size();
+    let rank = world.rank();
+
+    if size != 6 {
+        panic!("Size is {} must be 6", size);
+    }
+
+    match rank {
+        0 => {
+            for x in 1..6 {
+                let msg=[x as f64];
+                world.process_at_rank(x).send(&msg[..]);
+            }   
+
+        }
+        1..=6 => {
+            let (msg, status) = world.any_process().receive_vec::<f64>();
+            println!("Process {} got message {:?}.\nStatus is: {:?}", rank, msg, status);
+                                            
+        }
+            _ => unreachable!()            
+    }
+
 }
