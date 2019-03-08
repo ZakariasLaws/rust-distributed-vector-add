d9133af6f387a96660bc13737e0ae519b6d1e356
ZakariasLaws <zakarias.nl@gmail.com>
2019-03-08 13:39:11 +0100

    Works with uneven distribution over nodes, added comments and cleaned up

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,67 @@ extern crate mpi;
 mod vector_extra;
 
 use mpi::traits::*;
+use mpi::Count;
 use vector_extra::Evec;
 use mpi::topology::SystemCommunicator;
+use mpi::datatype::PartitionMut;
+use std::env;
+use std::process::exit;
 
+/// Perform vector in a distributed setting, using MPI capabilities.
+///
+/// Will only use the number of elements equal to the length of the smallest array, excess
+/// elements will be dropped.
 fn distributed_vector_add(world: SystemCommunicator, vec1: Evec<i32>, vec2: Evec<i32>) -> Evec<i32>{
     let rank = world.rank(); // Identifier for node running THIS instance
     let size = world.size();
 
-    let mut result;
+    let smallest_len = if vec1.smaller(&vec2) {
+        vec1.size() as i32
+    } else {
+        vec2.size() as i32
+    };
+
+    // Calculate interval for this node
+    let start = (smallest_len * rank / size) as i32;
+    let len = (smallest_len * (rank + 1) / size) as i32 - (smallest_len * rank / size) as i32;
+
+    let msg: Vec<_> = (0..len)
+        .map(|x| vec1.vec[(x+start) as usize] + vec2.vec[(x+start) as usize])
+        .collect();
+
+    // Check if the array is dividable with the number of processes
+    let counts: Vec<Count> = match smallest_len % size == 0{
+        true => {
+            vec![len; size as usize]
+        }
+        false => {
+            let mut tmp_v = Vec::new();
+            for i in 0..size {
+                let tmp_l = (smallest_len * (i + 1) / size) as i32 - (smallest_len * i / size) as i32;
+                tmp_v.push(tmp_l);
+            }
+            tmp_v
+        }
+    };
+
+    let displs: Vec<Count> = counts
+        .iter()
+        .scan(0, |acc, &x| {
+            let tmp = *acc;
+            *acc += x;
+            Some(tmp)
+        })
+        .collect();
+
+    let mut buf = vec![0; smallest_len as usize];
 
     {
-        let smallest_len = if vec1.smaller(&vec2) {
-            vec1.size() as i32
-        } else {
-            vec2.size() as i32
-        };
-
-        // Calculate interval for this node
-        let start = (smallest_len * rank / size) as i32;
-        let len = (smallest_len * (rank + 1) / size) as i32 - (smallest_len * rank / size) as i32;
-        let end = start + len;
-
-        result = Evec::new();
-        for _ in 0..smallest_len{
-            result.vec.push(0);
-        }
-
-        // Perform vector addition on interval partition
-        for i in 0..smallest_len {
-            if i >= start && i < end {
-                result.vec[i as usize] = vec1.vec[i as usize] + vec2.vec[i as usize];
-            }
-        }
-
-        world.process_at_rank(rank).broadcast_into(
-            &mut result.vec[start as usize..end as usize]);
+        let mut partition = PartitionMut::new(&mut buf[..], counts, &displs[..]);
+        world.all_gather_varcount_into(&msg[..], &mut partition);
     }
 
-    result
+    Evec { vec: buf}
 }
 
 fn main() {
@@ -48,22 +71,35 @@ fn main() {
     let world = universe.world();
     let root = if universe.world().rank() == 0 { true } else { false };
 
-    let mut vec1 = Evec::new();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        if root {
+            println!("Please provide an array length:\n\
+            Usage: mpirun ARGS vector_add <array_length>\n---------------------\n");
+        }
+        exit(1);
+    }
 
+    let array_length = args[1].parse().unwrap();
+
+    // Create two vectors and fill them with incrementing values from 0..<user_input>
+    let mut vec1 = Evec::new();
     let mut vec2 = Evec::new();
 
-    for x in 0..100 {
+    for x in 0..array_length {
         vec1.vec.push(x);
         vec2.vec.push(x);
     }
 
     if root {
-        println!("Running distributed vector add on {} nodes", world.size());
+        println!("Running distributed vector add on {} nodes.",
+                 world.size());
     }
 
     let result = distributed_vector_add(world, vec1, vec2);
 
     if root {
-        println!("The first 10 values in the resulting array are:\n{:?}", &result.vec[0..10]);
+        let length = if array_length < 30 {array_length} else {30};
+        println!("The first 30 elements in the resulting array are:\n{:?}", &result.vec[0..length as usize]);
     }
 }
--- src/vector_extra.rs
+++ src/vector_extra.rs
@@ -1,10 +1,27 @@
 use std::ops::{Add};
 
+/// Structure holding a vector with any type of elements.
 #[derive(Debug, PartialEq)]
 pub struct Evec<T> {
     pub vec: Vec<T>,
 }
 
+/// Add two Evec structures together by internally performing a
+/// vector addition
+///
+/// '''
+/// # Examples
+///
+/// let mut vec1: Evec<i32> = Evec<i32>;
+/// let mut vec2: Evec<i32> = Evec::new();
+///
+/// vec1.vec.push(1);
+/// vec2.vec.push(2);
+///
+/// let mut vec3 = vec1 + vec2;
+///
+/// assert_eq!(vec3, [3]);
+/// '''
 impl<T> Add<Evec<T>> for Evec<T>
     where T: Add<Output=T> + Copy {
 
@@ -31,19 +48,28 @@ impl<T> Add<Evec<T>> for Evec<T>
 }
 
 impl <T> Evec<T>{
+    /// Generate a new Evec<T>
     pub fn new() -> Evec<T>{
         Evec { vec: Vec::new()}
     }
 
+    /// Get the length of the internal vector
     pub fn size(&self) -> usize{
         self.vec.len()
     }
 
+    /// Compare to another Evec 'other' to check if internal
+    /// vector is smaller than the one from 'other'
     pub fn smaller(&self, other: &Evec<T>) -> bool{
         if self.size() < other.size() {
             return true;
         }
 
         return false;
+    }
+
+    /// Get the length of the inner vector
+    pub fn len(self) -> usize {
+        return self.vec.len();
     }
 }
